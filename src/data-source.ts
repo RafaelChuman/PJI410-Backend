cf605af90c02208777a8cc9dee81920794aaa188
Rafael Chuman <rafael_chumansantana@outlook.com>
2023-05-26 00:12:40 -0300

    Config for Neon Postgres Server

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,30 +8,43 @@ import { ERs } from "./entity/ERs/ERs";
 import { OilMonitor } from "./entity/OilMonitor/oilMonitor";
 
 export const PostgresDS = new DataSource({
-    type: "postgres",
-    host: "127.0.0.1",
-    port: 5432,
-    username: "chuman",
-    password: "pji410_univesp!",
-    database: "postgres",
-    name: "default",
-    entities: [Users, Collaborators, Activities, Zones, ERs, LubrificationSystemServices, OilMonitor],
-    migrations: ["./src/migration/*.m.ts"],
-
-})
+  // type: "postgres",
+  // host: "127.0.0.1",
+  // port: 5432,
+  // username: "chuman",
+  // password: "pji410_univesp!",
+  // database: "postgres",
+  // name: "default",
+  type: "postgres",
+  host: "ep-square-frog-051483.us-east-2.aws.neon.tech",
+  port: 5432,
+  username: "RafaelChuman",
+  password: "e9YBvLoHjNx6",
+  database: "neondb",
+  name: "default",
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+  entities: [
+    Users,
+    Collaborators,
+    Activities,
+    Zones,
+    ERs,
+    LubrificationSystemServices,
+    OilMonitor,
+  ],
+  migrations: ["./src/migration/*.m.ts"],
+});
 
 PostgresDS.initialize()
-    .then(() => {
+  .then(() => {
+    console.log(PostgresDS.options.migrations);
+    console.log(PostgresDS.migrations.length);
 
-        console.log(PostgresDS.options.migrations)
-        console.log(PostgresDS.migrations.length)
-
-        console.log("Database Initialized");
-    })
-    .catch((error: string) => console.log(error))
-
-
-
-
-
-
+    console.log("Database Initialized");
+  })
+  .catch((error: string) => console.log(error));
